c6ff5f5e952842c962aa036e79da4bf52c08cf6a
ana <anamolinari@gmail.com>
2025-09-08 15:25:55 +0200

    update: theme-color support for safari

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  // O CÓDIGO ACIMA É O MESMO CÓDIGO ABAIXO, PORÉM SIMPLIFICADO.
-  // if(html.classList.contains('light')) {
-  //  html.classList.remove('light')
-  // } else {
-  //  html.classList.add('light')
-  // }
 
-  // Pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // Subsituir a imagem
   if (html.classList.contains("light")) {
-    // Se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-ana-light.png")
     img.setAttribute(
       "alt",
       "Foto de Ana Molinari em preto e branco, de frente, sorrindo, cabelo preso e camisa branca"
     )
+    document
+      .querySelector('meta[name-"theme-color"]')
+      .setAttribute("content", "#FFFFFF")
   } else {
-    // Se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar-ana.png")
     img.setAttribute(
       "alt",
       "Foto de Ana Molinari em preto e branco, rosto inclinado, sorrindo, cabelo preso e camisa branca"
     )
+
+    document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute("content", "#000000")
   }
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const html = document.documentElement
+    const meta = document.querySelector('meta[name="theme-color"]')
+
+    if (html.classList.contains("light")) {
+      meta.setAttribute("content", "#ffffff")
+    } else {
+      meta.setAttribute("content", "#000000")
+    }
+  })
+
 }
